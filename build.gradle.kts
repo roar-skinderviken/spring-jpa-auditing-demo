a8dae6733237a89d55ef36e70a7b75901ec5ea4b
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-07-03 17:34:27 +0200

    Some cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,50 +2,45 @@ group = "no.mycompany"
 version = "0.0.1-SNAPSHOT"
 
 plugins {
-	java
-	alias(libs.plugins.springframework.boot)
-	alias(libs.plugins.spring.dependency.management)
-	alias(libs.plugins.freefair.lombok)
+    java
+    alias(libs.plugins.springframework.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.freefair.lombok)
 }
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	runtimeOnly("com.h2database:h2") // fallback db when running the app
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2") // fallback db when running the app
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:mysql")
-	testRuntimeOnly("org.flywaydb:flyway-mysql")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testRuntimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:mysql")
+    testRuntimeOnly("org.flywaydb:flyway-mysql")
+    testRuntimeOnly("com.mysql:mysql-connector-j")
 }
 
 // https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html#0.3
 val mockitoAgent = configurations.create("mockitoAgent")
 
 dependencies {
-	testImplementation(libs.mockito)
-	mockitoAgent(libs.mockito) { isTransitive = false }
+    testImplementation(libs.mockito)
+    mockitoAgent(libs.mockito) { isTransitive = false }
 }
 
-tasks.withType<Test> {
-	jvmArgs("-javaagent:${mockitoAgent.asPath}")
-	useJUnitPlatform()
+tasks.test {
+    jvmArgs(
+        "-javaagent:${mockitoAgent.asPath}",
+        "-Xshare:off"
+    )
+    useJUnitPlatform()
 }
